bd6f93bfb134211f9a46019fda007389819dba2f
Madhu Yadav <madhu.yadav@liferay.com>
2023-02-06 14:36:07 +0530

    DEV: using same jdk for jenkins as cloud dev

--- gs-jenkins-pipeline.sh
+++ gs-jenkins-pipeline.sh
@@ -7,6 +7,8 @@ cd /home/liferay/callisto/lfrgs-parkland-sol/webserver
 lcp deploy --project=callisto --environment=dev
 
 #Deploy liferay related changes
+export JAVA_HOME=/home/liferay/zulu11.62.17-ca-jdk11.0.18-linux_x64
+export PATH=$JAVA_HOME/bin:$PATH
 cd /home/liferay/callisto/lfrgs-parkland-sol/liferay 
 ./gradlew clean deploy createDockerfile
 cp LCP.json build/docker
