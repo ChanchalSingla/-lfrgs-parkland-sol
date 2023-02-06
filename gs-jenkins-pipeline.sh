697113eb0c90123aeb21d305b4b5afd32be66e78
Madhu Yadav <madhu.yadav@liferay.com>
2023-02-06 15:26:39 +0530

    DEV: added console logs to pipeline

--- gs-jenkins-pipeline.sh
+++ gs-jenkins-pipeline.sh
@@ -1,12 +1,12 @@
-#Pull latest code
+echo "Pulling latest code from https://github.com/liferay/lfrgs-parkland-sol/tree/develop"
 cd /home/liferay/callisto/lfrgs-parkland-sol
 git pull
 
-#Deploy webserver related changes
+echo "Deploy webserver related changes to https://console.liferay.cloud/projects/callisto-dev/services/webserver"
 cd /home/liferay/callisto/lfrgs-parkland-sol/webserver
-lcp deploy --project=callisto --environment=dev
+/home/liferay/lcp deploy --project=callisto --environment=dev
 
-#Deploy liferay related changes
+echo "Deploy liferay related changes to https://console.liferay.cloud/projects/callisto-dev/services/liferay"
 export JAVA_HOME=/home/liferay/zulu11.62.17-ca-jdk11.0.18-linux_x64
 export PATH=$JAVA_HOME/bin:$PATH
 cd /home/liferay/callisto/lfrgs-parkland-sol/liferay 
