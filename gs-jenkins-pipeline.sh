e3a0dd0ca0154b48e93abb45299ff4412a8f4895
Biswajit Behera <115999439+BiswajitBehera12@users.noreply.github.com>
2023-04-06 16:19:45 +0530

    DEV: Deploying all services for cloud upgrade using GS Jenkins except ci service on 6th April 2023.

--- gs-jenkins-pipeline.sh
+++ gs-jenkins-pipeline.sh
@@ -2,10 +2,17 @@ echo "Pulling latest code from https://github.com/liferay/lfrgs-parkland-sol/tre
 cd /home/liferay/callisto/lfrgs-parkland-sol
 git pull
 
-#8Feb: Disabling webserver deployments temporarily
-#echo "Deploy webserver related changes to https://console.liferay.cloud/projects/callisto-dev/services/webserver"
-#cd /home/liferay/callisto/lfrgs-parkland-sol/webserver
-#/home/liferay/lcp deploy --project=callisto --environment=dev
+echo "Deploy database related changes to https://console.liferay.cloud/projects/callisto-dev/services/database"
+cd /home/liferay/callisto/lfrgs-parkland-sol/database
+/home/liferay/lcp deploy --project=callisto --environment=dev
+
+echo "Deploy search related changes to https://console.liferay.cloud/projects/callisto-dev/services/search"
+cd /home/liferay/callisto/lfrgs-parkland-sol/search
+/home/liferay/lcp deploy --project=callisto --environment=dev
+
+echo "Deploy backup related changes to https://console.liferay.cloud/projects/callisto-dev/services/backup"
+cd /home/liferay/callisto/lfrgs-parkland-sol/backup
+/home/liferay/lcp deploy --project=callisto --environment=dev
 
 echo "Deploy liferay related changes to https://console.liferay.cloud/projects/callisto-dev/services/liferay"
 export JAVA_HOME=/home/liferay/zulu11.62.17-ca-jdk11.0.18-linux_x64
@@ -14,4 +21,8 @@ cd /home/liferay/callisto/lfrgs-parkland-sol/liferay
 ./gradlew clean deploy createDockerfile
 cp LCP.json build/docker
 cd /home/liferay/callisto/lfrgs-parkland-sol/liferay/build/docker
+/home/liferay/lcp deploy --project=callisto --environment=dev
+
+echo "Deploy webserver related changes to https://console.liferay.cloud/projects/callisto-dev/services/webserver"
+cd /home/liferay/callisto/lfrgs-parkland-sol/webserver
 /home/liferay/lcp deploy --project=callisto --environment=dev
